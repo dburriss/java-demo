2f73211b8e4dafe3f91509b4845f2bb705f27317
Devon Burriss <devon.burriss@coolblue.nl>
2023-12-09 19:59:47 +0100

    Use JavaDemoObservability

--- src/main/java/nl/coolblue/javademo/JavaDemoApplication.java
+++ src/main/java/nl/coolblue/javademo/JavaDemoApplication.java
@@ -1,39 +1,16 @@
 package nl.coolblue.javademo;
 
-import com.timgroup.statsd.NonBlockingStatsDClientBuilder;
-import com.timgroup.statsd.StatsDClient;
-import nl.coolblue.javademo.TagBuilder.Tags;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class JavaDemoApplication {
-    static String domain = "demo";
-    static String app = "java-demo";
-    static Logger logger = LoggerFactory.getLogger(JavaDemoApplication.class);
-    public static void main(String[] args) {
-        var ns = String.format("%s.%s", domain, app);
-        StatsDClient statsDClient = new NonBlockingStatsDClientBuilder()
-                .prefix(ns)
-                .hostname("localhost")
-                .port(8125)
-                .build();
 
-        // metrics client
-        Metrics metrics = new StatsDMetrics(statsDClient);
-        var telemetry = new MonitoringEvents(logger, metrics);
-        // define tags array for metric
-        var tagsBuilder = Tags.success().withEnv("prod").withService("JavaDemo").withVersion("1.0.0");
-        var tags = tagsBuilder.build();
-        // send metric
-        telemetry.getMetrics().event("Java Demo app started", "Running in prod", tags);
-        telemetry.getMetrics().histogram("app_started", 1L, tags);
-        telemetry.getLogger().info("Java Demo app starting!!!");
+    public static void main(String[] args) {
         
+        var observability = new JavaDemoObservability();
+        observability.applicationStarted();
         SpringApplication.run(JavaDemoApplication.class, args);
-        
     }
 
 }
---
+++ src/main/java/nl/coolblue/javademo/JavaDemoObservability.java
@@ -0,0 +1,56 @@
+package nl.coolblue.javademo;
+
+import com.timgroup.statsd.NonBlockingStatsDClientBuilder;
+import com.timgroup.statsd.StatsDClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JavaDemoObservability {
+    static String domain = "demo";
+    static String app = "java-demo";
+    private final MonitoringEvents monitoringEvents;
+    private final String env;
+    private final String version;
+
+    public JavaDemoObservability() {
+        var ns = String.format("%s.%s", domain, app);
+        StatsDClient statsDClient = new NonBlockingStatsDClientBuilder()
+            .prefix(ns)
+            .hostname("localhost")
+            .port(8125)
+            .build();
+        
+        // get env from settings
+        env = "prod";
+        
+        // get version from settings
+        version = "1.0.0";
+
+        // metrics client
+        Metrics metrics = new StatsDMetrics(statsDClient);
+        
+        // logger
+        Logger logger = LoggerFactory.getLogger(JavaDemoApplication.class);
+        
+        // monitoring events
+        monitoringEvents = new MonitoringEvents(logger, metrics);
+    }
+    
+    private void addDefaultTags(TagBuilder tagBuilder) {
+        tagBuilder.withEnv(env).withService(app).withVersion(version);
+    }
+    
+    public void applicationStarted() {
+        // define tags array for metric
+        var tagsBuilder = TagBuilder.Tags.success();
+        addDefaultTags(tagsBuilder);
+        var tags = tagsBuilder.build();
+        
+        // send metrics
+        monitoringEvents.getMetrics().event("Java Demo app started", "Running in prod", tags);
+        monitoringEvents.getMetrics().histogram("app_started", 1L, tags);
+        
+        // send log
+        monitoringEvents.getLogger().info("Java Demo app starting!!!");
+    }
+}
