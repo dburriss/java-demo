0cc54e706c13428726ef96c267ec9d9329c42a4b
Devon Burriss <devon.burriss@coolblue.nl>
2023-12-09 13:57:04 +0100

    Send start metric using metric client

--- src/main/java/nl/coolblue/javademo/JavaDemoApplication.java
+++ src/main/java/nl/coolblue/javademo/JavaDemoApplication.java
@@ -3,10 +3,27 @@ package nl.coolblue.javademo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.timgroup.statsd.NonBlockingStatsDClientBuilder;
+import com.timgroup.statsd.NonBlockingStatsDClient;
+import com.timgroup.statsd.StatsDClient;
+
 @SpringBootApplication
 public class JavaDemoApplication {
-
+    static String domain = "demo";
+    static String app = "java-demo";
     public static void main(String[] args) {
+        var ns = String.format("%s.%s", domain, app);
+        StatsDClient client = new NonBlockingStatsDClientBuilder()
+                .prefix(ns)
+                .hostname("localhost")
+                .port(8125)
+                .build();
+
+        // define tags array for metric
+        String[] tags = {"env:prod", "service:java-demo", "version:1.0.0"};
+        // send metric
+        client.histogram("app_started", 1L, tags);
+        
         SpringApplication.run(JavaDemoApplication.class, args);
     }
 
